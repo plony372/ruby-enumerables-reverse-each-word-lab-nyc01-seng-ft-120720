1cb6159d4fe28f003896f9db014a2f385612227e
plony benav <menachemfuterfas@gmail.com>
2020-11-15 20:41:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,10 @@
 def reverse_each_word 
   
 r = reverse_each_word
-myArray = r.split() # first turn the string in to an array so we can iterate over it
 reversed_stirng = []
-myArray.each{ |word |reversed_stirng.push(word.reverse)
-}
+
+myArray = r.split()
+
+myArray.each{ |word |reversed_stirng.push(word.reverse)}
 reversed_stirng.join(" ")
-  
-  
-  
 end
